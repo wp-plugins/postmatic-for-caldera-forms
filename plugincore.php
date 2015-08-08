afc2d8fadc1b4eae640a8b3d461b76bc3f93736b
vernal <vernal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-08 01:26:27 +0000

    Ok, an acutal plugin file.
    git-svn-id: https://plugins.svn.wordpress.org/postmatic-for-caldera-forms/trunk@1215259 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugincore.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* @package   Caldera_Forms_Postmatic
+* @author    Josh Pollock <Josh@CalderaWP.com>
+* @license   GPL-2.0+
+* @link      
+* @copyright 2015 Josh Pollock for CalderaWP
+*
+* @wordpress-plugin
+* Plugin Name: Postmatic For Caldera Forms
+* Plugin URI:  https://calderawp.com/downloads/postmatic-for-caldera-forms/
+* Description: Postmatic integration for Caldera Forms
+* Version:     1.0.0
+* Author:      Josh Pollock <Josh@CalderaWP.com>
+* Author URI:  http://calderawp.com
+* Text Domain: cf-postmatic
+* License:     GPL-2.0+
+* License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+* Domain Path: /languages
+*/
